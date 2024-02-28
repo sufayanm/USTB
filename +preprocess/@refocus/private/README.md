db4fc3670570e4449ffd7cdb8734f640ed600acd
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2024-02-28 16:56:55 +0100

    Add REFoCUS and examples to the USTB

---
+++ +preprocess/@refocus/private/README.md
@@ -0,0 +1,8 @@
+# Retrospective Encoding For Conventional Ultrasound Sequences (REFoCUS)
+
+Current methods:
+
+*  Adjoint (leave param empty for no ramp filter, use param to define one if desired)
+*  Truncated SVD (condition number)
+*  Regularized SVD (regularizing number)
+*  Tikhonov regularization (regularizing number)
---
+++ examples/REFoCUS/PW_picmus.m
@@ -0,0 +1,82 @@
+%% REFOCUS on PW data
+%
+%   $Last updated: 2017/09/15$
+
+%% Getting the data
+%
+% We define the local path and the url where the data is stored
+
+% data location
+url='http://ustb.no/datasets/';      % if not found data will be downloaded from here
+filename='PICMUS_experiment_contrast_speckle.uff';
+
+% checks if the data is in your data path, and downloads it otherwise.
+% The defaults data path is under USTB's folder, but you can change this
+% by setting an environment variable with setenv(DATA_PATH,'the_path_you_want_to_use');
+tools.download(filename, url, data_path);   
+
+channel_data=uff.read_object([data_path filesep filename],'/channel_data');
+scan=uff.read_object([data_path filesep filename],'/scan');
+
+
+%% Beamforming
+%
+% We define a beamformer, and the corresponding transmit and apodization
+% windows, and launch it.
+
+pipe=pipeline();
+pipe.channel_data=channel_data;
+pipe.scan=scan;
+    
+% receive apodization
+pipe.receive_apodization.window=uff.window.tukey50;
+pipe.receive_apodization.f_number=1.7;
+
+% transmit apodization
+pipe.transmit_apodization.window=uff.window.tukey50;
+pipe.transmit_apodization.f_number=1.7;
+
+% launch beamforming
+b_data_new=pipe.go({midprocess.das postprocess.coherent_compounding});
+
+%%
+channel_data.data = single(channel_data.data);
+
+%% Comparing results
+%
+% We plot both images side by side.
+
+figure;
+b_data.plot(subplot(1,2,1),'Original');
+b_data_new.plot(subplot(1,2,2),'New');
+set(gcf,'Position',[100   100   750   450])
+
+% Run REFOCUS preprocess
+tic
+refocus = preprocess.refocus()
+refocus.input = channel_data;
+refocus.use_filter = 0;
+refocus.regularization = @Hinv_tikhonov;
+refocus.decode_parameter = 1e-1;
+refocus.post_pad_samples = 0;
+channel_data_REFOCUS = refocus.go()
+toc()
+
+%%
+demod = preprocess.fast_demodulation()
+%demod.modulation_frequency = 2.5*10^6;
+demod.plot_on = true
+demod.input = channel_data_REFOCUS;
+channel_data_REFOCUS_demod = demod.go()
+
+%% Do beamforming
+mid_REFOCUS = midprocess.das()
+mid_REFOCUS.channel_data=channel_data_REFOCUS_demod;
+mid_REFOCUS.dimension = dimension.both();
+mid_REFOCUS.scan=scan;
+mid_REFOCUS.code = code.mex;
+mid_REFOCUS.transmit_apodization.window=uff.window.none;
+mid_REFOCUS.receive_apodization.window=uff.window.none;
+mid_REFOCUS.receive_apodization.f_number=1;
+b_data_delayed_REFOCUS = mid_REFOCUS.go()
+b_data_delayed_REFOCUS.plot();
